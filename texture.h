e82be8e15b007114f06a07e1385443ce0fbe08c4
Mayank Barad <mbarad0906@gmail.com>
2025-04-27 09:51:20 -0400

    Added texture.h, won't work without this file

---
+++ texture.h
@@ -0,0 +1,53 @@
+#ifndef _TEXTURE_H_
+#define _TEXTURE_H_
+
+#include <memory>
+
+#include "texture.h"
+#include "color.h"
+
+class texture {
+    public:
+        virtual ~texture() = default;
+
+        virtual color value(double u, double v, const point3& p) const = 0;
+};
+
+class solid_color : public texture {
+    public:
+        solid_color(const color& albedo) : albedo(albedo) {}
+
+        solid_color(double red, double green, double blue) : solid_color(color(red,green,blue)) {}
+
+        color value(double u, double v, const point3& p) const override {
+            return albedo;
+        }
+
+    private:
+        color albedo;
+};
+
+class checker_texture : public texture {
+    public:
+        checker_texture(double scale, std::shared_ptr<texture> even, std::shared_ptr<texture> odd) 
+            : inv_scale(1.0 / scale), even(even), odd(odd) {}
+        checker_texture(double scale, const color& c1, const color& c2)
+            : checker_texture(scale, std::make_shared<solid_color>(c1), std::make_shared<solid_color>(c2)) {}
+        color value(double u, double v, const point3& p) const override {
+            // return color(0.5, 0.5, 0.5);
+            auto xInt = int(std::floor(inv_scale * p.x()));
+            auto yInt = int(std::floor(inv_scale * p.y()));
+            auto zInt = int(std::floor(inv_scale * p.z()));
+            
+            bool isEven = (xInt + yInt + zInt) % 2 == 0;
+
+            return isEven ? even->value(u, v, p) : odd->value(u, v, p);
+        }
+    
+    private:
+        double inv_scale;
+        std::shared_ptr<texture> even;
+        std::shared_ptr<texture> odd;
+};
+
+#endif
